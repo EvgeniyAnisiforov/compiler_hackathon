a07f0db205a6666348f3ce456551e0ef4ced50ad
Evgeniy Anisiforov <y.anisiforov@mail.ru>
2024-05-14 01:42:45 +0300

    first base routing

--- frontend/compiler_hackathon/src/App.tsx
+++ frontend/compiler_hackathon/src/App.tsx
@@ -1,7 +1,29 @@
-import './App.css'
+import "./App.css"
+import { Link, Route, Routes, useNavigate } from "react-router-dom"
+import { useEffect } from "react"
+
+import { LoginPage } from "./pages/LoginPage"
+import { RegistrationPage } from "./pages/RegistrationPage"
+import { HomePage } from "./pages/HomePage"
+import { NotFoundPage } from "./pages/NotFoundPage"
 
 function App() {
-  return <div></div>
+
+  const navigate = useNavigate();
+
+     useEffect(() => {
+       navigate('/'); 
+     }, []); 
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/registration" element={<RegistrationPage />} />
+        <Route path="/home" element={<HomePage />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </>
+  )
 }
 
 export default App
