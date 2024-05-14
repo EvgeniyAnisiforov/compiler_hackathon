e3a9eeabd8d161f56615e05a16804f2bb553ba07
Evgeniy Anisiforov <y.anisiforov@mail.ru>
2024-05-14 16:33:39 +0300

    create page login

---
+++ frontend/compiler_hackathon/src/pages/Home/HomePage.tsx
@@ -0,0 +1,6 @@
+import { FC, ReactElement } from "react"
+
+const HomePage: FC<{}> = (): ReactElement => {
+    return <h1>fff</h1>
+}
+export {HomePage}
---
+++ frontend/compiler_hackathon/src/pages/NotFound/NotFoundPage.tsx
@@ -0,0 +1,6 @@
+import { FC, ReactElement } from "react"
+
+const NotFoundPage: FC<{}> = (): ReactElement => {
+    return <h1>что-то пошло не так</h1>
+}
+export {NotFoundPage}
