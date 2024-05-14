71d8eb283ca4f7332fa6c19fe91f49cea6930c07
Evgeniy Anisiforov <y.anisiforov@mail.ru>
2024-05-14 20:03:08 +0300

    fixed page login and registration

--- frontend/compiler_hackathon/src/pages/Login/LoginPage.tsx
+++ frontend/compiler_hackathon/src/pages/Login/LoginPage.tsx
@@ -8,16 +8,31 @@ const LoginPage: FC<{}> = (): ReactElement => {
     const navigate = useNavigate()
     const goRegistr = () => navigate("/registration")
     const goHome = () => navigate("/home")
-    return <div className={style["LoginPage__wrapper"]}>
-        <div className={style["LoginPage__container--white"]}>
-            <h3 className={style["LoginPage__h1"]}>Авторизация</h3>
-            <div className={style["LoginPage__containerInput"]}><Input placeholder="Логин" /></div>
-            <div className={style["LoginPage__containerInput"]}><Input type="password" placeholder="Пароль" /></div>
-            <div className={style["LoginPage__containerText"]}><p>У вас нет аккаунта?<button className={style["LoginPage__buttonRegistr"]} onClick={goRegistr}>Регистрация</button></p></div>
-            <div className={style["LoginPage__containerTextGuest"]}><a onClick={goHome}>Войти как гость</a></div>
-            <button className={style["LoginPage__button"]}>Войти</button>
-        </div>
-    </div>
+
+    return (
+        <div className={style["area"]} >
+            <div className={style["LoginPage__container--white"]}>
+                <h3 className={style["LoginPage__h1"]}>Авторизация</h3>
+                <div className={style["LoginPage__containerInput"]}><Input placeholder="Логин" /></div>
+                <div className={style["LoginPage__containerInput"]}><Input type="password" placeholder="Пароль" /></div>
+                <div className={style["LoginPage__containerText"]}><p>У вас нет аккаунта?<button className={style["LoginPage__buttonRegistr"]} onClick={goRegistr}>Регистрация</button></p></div>
+                <div className={style["LoginPage__containerTextGuest"]}><a onClick={goHome}>Войти как гость</a></div>
+                <button className={style["LoginPage__button"]}>Войти</button>
+            </div>
+            <ul className={style["circles"]}>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+            </ul>
+        </div >
+    )  
 }
 export {LoginPage}
 
