9f0efb6899f3021bb124a7f28598b79d538e4be0
Evgeniy Anisiforov <y.anisiforov@mail.ru>
2024-05-14 23:32:14 +0300

    dont't touch, add button

--- frontend/compiler_hackathon/src/pages/Home/HomePage.tsx
+++ frontend/compiler_hackathon/src/pages/Home/HomePage.tsx
@@ -1,9 +1,20 @@
 import { FC, ReactElement } from "react"
+import { GiHamburgerMenu } from "react-icons/gi";
 import style from "./HomePage.module.css"
 
 const HomePage: FC<{}> = (): ReactElement => {
     return <div>
         <div className={style["area"]} >
+            <div className={style["HomePage__wrapper"]}>
+                <div className={style["Homepage__containerHeader--flex"]}>
+                    <div><GiHamburgerMenu className={style["HomePage__burgerMenu"]}/></div>
+                    <div>
+                        <button className={style["HomePage__button"]}>Войти</button>
+                        <button className={style["HomePage__button"]}>Регистрация</button>
+                    </div>
+                </div>
+            </div>
+
             <ul className={style["circles"]}>
                 <li></li>
                 <li></li>
