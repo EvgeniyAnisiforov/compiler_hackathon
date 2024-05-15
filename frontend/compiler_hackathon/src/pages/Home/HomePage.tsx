678154318b5eaf53bc39f947f3d197fd88759ad3
Evgeniy Anisiforov <y.anisiforov@mail.ru>
2024-05-15 19:01:36 +0300

    доработал страницу с окном компилятора, настроил синхронизацию скроллбаров

--- frontend/compiler_hackathon/src/pages/Home/HomePage.tsx
+++ frontend/compiler_hackathon/src/pages/Home/HomePage.tsx
@@ -1,5 +1,5 @@
 import { FC, ReactElement } from "react"
-import { useState } from "react";
+import { useState, useRef, useEffect  } from "react";
 import { GiHamburgerMenu } from "react-icons/gi";
 import { useNavigate } from "react-router-dom";
 import style from "./HomePage.module.css"
@@ -12,7 +12,8 @@ const HomePage: FC<{}> = (): ReactElement => {
 
     const [code, setCode] = useState('');
     const codeLines = code.split('\n'); 
-
+    const textAreaRef = useRef<HTMLTextAreaElement>(null); // Уточнили тип рефа
+    const lineNumbersRef = useRef<HTMLDivElement>(null);     // Уточнили тип рефа
 
     const handlePaste = (event: React.ClipboardEvent<HTMLTextAreaElement>) => {
         event.preventDefault();
@@ -22,6 +23,24 @@ const HomePage: FC<{}> = (): ReactElement => {
         const formattedText = pastedText.replace(/\t/g, '  '); 
         setCode(code + formattedText);
     }
+
+    useEffect(() => {
+        const handleScroll = () => {
+          if (lineNumbersRef.current && textAreaRef.current) {
+            lineNumbersRef.current.scrollTop = textAreaRef.current.scrollTop;
+          }
+        };
+    
+        if (textAreaRef.current) {
+          textAreaRef.current.addEventListener('scroll', handleScroll);
+        }
+    
+        return () => {
+          if (textAreaRef.current) {
+            textAreaRef.current.removeEventListener('scroll', handleScroll);
+          }
+        };
+      }, []);
 
     return (
         <div className={style["area"]} >
@@ -35,19 +54,32 @@ const HomePage: FC<{}> = (): ReactElement => {
                 </div>
 
 
-                <div className={style["HomePage__compilerWrapper"]} style={{ display: 'flex', justifyContent: "center" }}>
-                    <div className={style["HomePage__compilerNumber"]}>
-                        {codeLines.map((_, index) => (
-                        <div style={{fontFamily:"monospace"}} key={index}>{index + 1}</div> 
-                        ))}
+                <div className={style["HomePage__containerCompiler--flex"]}>
+                    <div className={style["HomePage__compilerWrapper"]} style={{ display: 'flex', justifyContent: "center" }}>
+                        <div className={style["HomePage__compilerNumber"]}>
+                            <div ref={lineNumbersRef} className={style["HomePage__compilerNumber--margin"]}>
+                                {codeLines.map((_, index) => (
+                                <div style={{fontFamily:"monospace"}} key={index}>{index + 1}</div> 
+                                ))}
+                            </div>
+                        </div>
+                        <div className={style["HomePage__containerCompilerTextCode"]}>
+                            <textarea
+                                    ref={textAreaRef}
+                                    value={code}
+                                    onChange={(e) => setCode(e.target.value)}
+                                    onPaste={handlePaste}
+                                    className={style["HomePage__compilerTextCode"]}
+                                />
+                        </div>
                     </div>
-                        <textarea
-                            value={code}
-                            onChange={(e) => setCode(e.target.value)}
-                            onPaste={handlePaste}
-                            className={style["HomePage__compilerTextCode"]}
-                        />
+
+                    <div className={style["HomePage__containerOutput"]}>
+                        <div className={style["HomePage__compilerWrapperOutput"]}>
+                            Нажмите запустить, что бы увидеть результат
+                        </div>
                     </div>
+                </div>
             </div>
 
             <ul className={style["circles"]}>
