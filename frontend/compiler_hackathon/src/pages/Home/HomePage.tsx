8d14c7a1a5f5090cca9968cd0acd13766f3f8a61
Evgeniy Anisiforov <y.anisiforov@mail.ru>
2024-05-15 03:04:21 +0300

    верстка окна компилятора

--- frontend/compiler_hackathon/src/pages/Home/HomePage.tsx
+++ frontend/compiler_hackathon/src/pages/Home/HomePage.tsx
@@ -1,18 +1,53 @@
 import { FC, ReactElement } from "react"
+import { useState } from "react";
 import { GiHamburgerMenu } from "react-icons/gi";
+import { useNavigate } from "react-router-dom";
 import style from "./HomePage.module.css"
 
 const HomePage: FC<{}> = (): ReactElement => {
-    return <div>
+
+    const navigate = useNavigate()
+    const goRegistr = () => navigate("/registration")
+    const goHome = () => navigate("/")
+
+    const [code, setCode] = useState('');
+    const codeLines = code.split('\n'); 
+
+
+    const handlePaste = (event: React.ClipboardEvent<HTMLTextAreaElement>) => {
+        event.preventDefault();
+        const clipboardData = event.clipboardData; //  Убрали window.clipboardData
+        const pastedText = clipboardData.getData('text');
+
+        const formattedText = pastedText.replace(/\t/g, '  '); 
+        setCode(code + formattedText);
+    }
+
+    return (
         <div className={style["area"]} >
             <div className={style["HomePage__wrapper"]}>
                 <div className={style["Homepage__containerHeader--flex"]}>
                     <div><GiHamburgerMenu className={style["HomePage__burgerMenu"]}/></div>
                     <div>
-                        <button className={style["HomePage__button"]}>Войти</button>
-                        <button className={style["HomePage__button"]}>Регистрация</button>
+                        <button onClick={goHome} className={style["HomePage__button"]}>Войти</button>
+                        <button onClick={goRegistr}className={style["HomePage__button"]}>Регистрация</button>
                     </div>
                 </div>
+
+
+                <div className={style["HomePage__compilerWrapper"]} style={{ display: 'flex', justifyContent: "center" }}>
+                    <div className={style["HomePage__compilerNumber"]}>
+                        {codeLines.map((_, index) => (
+                        <div style={{fontFamily:"monospace"}} key={index}>{index + 1}</div> 
+                        ))}
+                    </div>
+                        <textarea
+                            value={code}
+                            onChange={(e) => setCode(e.target.value)}
+                            onPaste={handlePaste}
+                            className={style["HomePage__compilerTextCode"]}
+                        />
+                    </div>
             </div>
 
             <ul className={style["circles"]}>
@@ -28,6 +63,6 @@ const HomePage: FC<{}> = (): ReactElement => {
                 <li></li>
             </ul>
         </div >
-    </div>
+    )
 }
 export {HomePage}
