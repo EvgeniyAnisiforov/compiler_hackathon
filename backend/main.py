1bc8f26a9a553a985f1b3a6ca3db05b3e192d75d
Ilya <iosipov@ptsecurity.com>
2024-05-22 02:50:33 +0300

    back full

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+from sqlalchemy.testing.provision import create_db
+
 from routes.compiler_routes import router as compiler_router
 from routes.auth_routes import router as auth_router
 from routes.statistic_router import router as statistic_router
@@ -18,11 +20,6 @@ app.include_router(compiler_router, prefix="/compile")
 app.include_router(auth_router, prefix="/users")
 app.include_router(statistic_router, prefix="/statistic")
 
+
 if __name__ == "__main__":
-    # @app.on_event("startup")
-    # async def startup():
-    #     await database.connect()
-    # @app.on_event("shutdown")
-    # async def shutdown():
-    #     await database.disconnect()
     uvicorn.run("main:app", host="localhost", port=8000, reload=True, log_level="info")
