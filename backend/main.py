758d487eaf73d633afdcc43ee74bf2ba34c8d23e
Evgeniy Anisiforov <y.anisiforov@mail.ru>
2024-05-22 13:49:43 +0300

    настроил CORS

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from sqlalchemy.testing.provision import create_db
+from fastapi.middleware.cors import CORSMiddleware
 
 from routes.compiler_routes import router as compiler_router
 from routes.auth_routes import router as auth_router
@@ -16,10 +16,24 @@ app = FastAPI(
     root_path='/api'
 )
 
+# Настройка CORS
+origins = [
+    "http://localhost:5173", # Домен вашего фронтенда
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(compiler_router, prefix="/compile")
 app.include_router(auth_router, prefix="/users")
 app.include_router(statistic_router, prefix="/statistic")
 
-
 if __name__ == "__main__":
     uvicorn.run("main:app", host="localhost", port=8000, reload=True, log_level="info")
+
+
